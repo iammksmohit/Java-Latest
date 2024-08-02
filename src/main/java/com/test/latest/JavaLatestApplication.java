9aa64fe3b43a820f0071cc452e461f186894c922
iammksmohit <mohitchemistry123@gmail.com>
2024-08-02 22:21:51 +0530

    first commit

---
+++ src/main/java/com/test/latest/JavaLatestApplication.java
@@ -0,0 +1,13 @@
+package com.test.latest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLatestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaLatestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/latest/JavaLatestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.latest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLatestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
